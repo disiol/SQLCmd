40b9ff8d1fad2d3d2b052ceb27890337f1a789a1
denis <deoniisii@gmail.com>
2017-11-11 16:54:06 +0200

    создал комксласс команды DatabaseNames и тест кнему реализовал показ списка таблиц в движке

--- src/main/java/ua/com/denisimusIT/SQLCmd/controller/commands/DatabaseNames.java
+++ src/main/java/ua/com/denisimusIT/SQLCmd/controller/commands/DatabaseNames.java
@@ -1,7 +1,7 @@
 package ua.com.denisimusIT.SQLCmd.controller.commands;
 
 public class DatabaseNames {
-
+     //TODO команда для отоброжения списка базы данных
     //datname : [template1, template0, postgres, sqlCmd]
 
 
