89fa347442c1db602731495c866e63f3bfd45f0c
denis <deoniisii@gmail.com>
2017-11-14 22:47:00 +0200

    сделал MainControllerTest

--- src/main/java/ua/com/denisimusIT/SQLCmd/controller/MainController.java
+++ src/main/java/ua/com/denisimusIT/SQLCmd/controller/MainController.java
@@ -1,4 +1,5 @@
 package ua.com.denisimusIT.SQLCmd.controller;
 
 public class MainController {
+
 }
--- src/test/java/ua/com/denisimusIT/SQLCmd/controller/MainControllerTest.java
+++ src/test/java/ua/com/denisimusIT/SQLCmd/controller/MainControllerTest.java
@@ -2,9 +2,27 @@ package ua.com.denisimusIT.SQLCmd.controller;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class MainControllerTest {
+    final String newline = System.lineSeparator();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+
+    MainController mainController = new MainController();
+
     @Test
-    public void MainControllerTest(){
+    public void MainControllerTest() {
+        System.setOut(new PrintStream(outContent));
+
+        String expected = "Welcome to SQLCmd! =)" + newline +
+                "For connection to the bazedanykh you veddit the connect | database | username | password " +
+                "or help command for a help call";
+        String actual = outContent.toString();
+        assertEquals("MainControllerTest", expected, actual);
 
     }
 }
