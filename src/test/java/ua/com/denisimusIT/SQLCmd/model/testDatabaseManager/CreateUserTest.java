52a616ed9639eea9aa146f809be50a02596f8e5e
denis <deoniisii@gmail.com>
2017-11-18 21:36:58 +0200

    make public void dropDatabase()

--- src/test/java/ua/com/denisimusIT/SQLCmd/model/testDatabaseManager/CreateUserTest.java
+++ src/test/java/ua/com/denisimusIT/SQLCmd/model/testDatabaseManager/CreateUserTest.java
@@ -13,42 +13,64 @@ import static org.junit.Assert.assertEquals;
 
 public class CreateUserTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    final String newline = System.lineSeparator();
 
     DatabaseManager postgresDatabaseManager = new PostgresDatabaseManager();
+
+    String dataBase = "postgres";
+    String userDb = "postgres";
+    String password = "1111";
     private String testDatabase = "testdatabase";
+
+    String user;
     @Before
-    public void connectToDataBase() {
-        String dataBase = "postgres";
-        String user = "postgres";
-        String password = "1111";
-        postgresDatabaseManager.connect(dataBase, user, password);
-        //TODO создать и выбрать баззу
+
+    public void before(){
+        connectToDataBase();
+        CrateDb();
+        ConnectToCrateDb();
+
+    }
+
+    private void connectToDataBase() {
+        postgresDatabaseManager.connect(dataBase, userDb, password);
+        //создать и выбрать баззу
+    }
+
+    private void ConnectToCrateDb() { ;
+        postgresDatabaseManager.connect(testDatabase, userDb, password);
+    }
+
+    private void CrateDb() {
         postgresDatabaseManager.createDatabase(testDatabase);
-        postgresDatabaseManager.connect(testDatabase, user, password);
     }
 
     @Test
     public void createUserTest() {
         System.setOut(new PrintStream(outContent));
 
-        String user = "den";
+        user = "den";
         String password = "111";
         postgresDatabaseManager.createUser(user, password);
-        String expected = "It is created user:" + user + " with the password: " + password;
+        String expected = "Creating user..." + newline +
+                "It is created user: " + user + " with the password: " + password;
         String actual = outContent.toString();
         assertEquals("created user", expected, actual);
 
 
-        expected = "The user : " + user + "already is";
-        actual = outContent.toString();
-        assertEquals("The user already is", expected, actual);
+        String expected1 = "The user : " + user + "already is created";
+        String actual2 = outContent.toString();
+        assertEquals("The user already is created", expected, actual);
 
 
     }
 
 
     @After
-    public  void dropDatabase(){
+    public void dropDatabase() {
+        ConnectToCrateDb();
+        postgresDatabaseManager.dropUser(user);
+        connectToDataBase();
         postgresDatabaseManager.dropDatabase(testDatabase);
     }
 
