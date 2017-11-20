78a5fc94f85c5413cc4fa54d00626c627e3260e5
denis <deoniisii@gmail.com>
2017-11-20 12:25:04 +0200

    make tests and commands

---
+++ src/main/java/ua/com/denisimusIT/SQLCmd/controller/commands/create.java
@@ -0,0 +1,12 @@
+package ua.com.denisimusIT.SQLCmd.controller.commands;
+
+public class create {
+//•TODO Команда создает новую таблицу с заданными полями
+//•Формат: ua.com.denisimusIT.SQLCmd.controller.commands.create | tableName | column1 | column2 | ... | columnN
+//•где: tableName - имя таблицы
+//•column1 - имя первого столбца записи
+//•column2 - имя второго столбца записи
+//•columnN - имя n-го столбца записи
+//•Формат вывода: текстовое сообщение с результатом выполнения
+//операции
+    }
