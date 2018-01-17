301c40df3c090f5d400377b9792ce9d0dcd7ebb3
Denis <deoniisii@gmail.com>
2018-01-17 16:07:47 +0200

    refactorint IsConnected --> IsConnect

--- src/main/java/ua/com/denisimusIT/SQLCmd/controller/command/connektToDatabase/IsConnect.java
+++ src/main/java/ua/com/denisimusIT/SQLCmd/controller/command/connektToDatabase/IsConnect.java
@@ -4,18 +4,18 @@ import ua.com.denisimusIT.SQLCmd.controller.command.Command;
 import ua.com.denisimusIT.SQLCmd.model.DatabaseManager;
 import ua.com.denisimusIT.SQLCmd.view.View;
 
-public class IsConnected implements Command {
+public class IsConnect implements Command {
     private final View view;
     private final DatabaseManager manager;
 
-    public IsConnected(View view, DatabaseManager manager) {
+    public IsConnect(View view, DatabaseManager manager) {
         this.view = view;
         this.manager = manager;
     }
 
     @Override
     public boolean canProcess(String command) {
-        return manager.isConnected();
+        return !manager.isConnected();
     }
 
     @Override
---
+++ src/test/java/ua/com/denisimusIT/SQLCmd/controller/command/UnsupportedCommandTest.java
@@ -0,0 +1,63 @@
+package ua.com.denisimusIT.SQLCmd.controller.command;
+
+import org.junit.Before;
+import org.junit.Test;
+import ua.com.denisimusIT.SQLCmd.controller.command.connektToDatabase.IsConnect;
+import ua.com.denisimusIT.SQLCmd.model.DatabaseManager;
+import ua.com.denisimusIT.SQLCmd.view.View;
+
+import static junit.framework.TestCase.*;
+import static org.mockito.Mockito.*;
+
+public class UnsupportedComandTest {
+    View view;
+    DatabaseManager databaseManager;
+    Command isConnect;
+
+    @Before
+    public void setup() {
+        view = mock(View.class);
+        databaseManager = mock(DatabaseManager.class);
+        isConnect = new IsConnect(view, databaseManager);
+    }
+
+    @Test
+    public void canProcessTrueTest() {
+        //wen
+        boolean canProcess = isConnect.canProcess("ffff");
+
+        //then
+        assertTrue("canProcessTrueTest", canProcess);
+    }
+
+    @Test
+    public void canProcessFalseTest() {
+        when(databaseManager.isConnected()).thenReturn(true);
+        boolean canProcess = isConnect.canProcess("ffff");
+        assertFalse("canProcessFalseTest", canProcess);
+    }
+
+    @Test
+    public void processTest() {
+        String command = "help";
+        String message = String.format("You cannot use a command '%s' be not connected by means of a command yet " +
+                "connect|databaseName|userName|password", command);
+        //wen
+        isConnect.canProcess(command);
+        isConnect.process(command);
+        //then
+        verify(view).write(message);
+
+    }
+
+    @Test
+    public void descriptionTest() {
+        assertNull("descriptionTest", isConnect.description());
+    }
+
+    @Test
+    public void formatTest() {
+        assertNull("descriptionTest", isConnect.format());
+
+    }
+}
