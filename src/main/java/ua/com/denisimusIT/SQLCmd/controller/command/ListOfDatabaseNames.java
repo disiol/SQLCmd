e94bfc6581f79a4a6fec6779f7ac15fdfef935c9
Denis <deoniisii@gmail.com>
2018-01-24 17:14:15 +0200

    implements interface Command

--- src/main/java/ua/com/denisimusIT/SQLCmd/controller/command/ListOfDatabaseNames.java
+++ src/main/java/ua/com/denisimusIT/SQLCmd/controller/command/ListOfDatabaseNames.java
@@ -1,7 +1,26 @@
 package ua.com.denisimusIT.SQLCmd.controller.command;
 
-public class ListOfDatabaseNames {
-     //TODO команда для отоброжения списка базы данных
+public class ListOfDatabaseNames implements Command {
+    @Override
+    public boolean canProcess(String command) {
+        return false;
+    }
+
+    @Override
+    public void process(String command) {
+
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
+
+    @Override
+    public String format() {
+        return null;
+    }
+    //TODO команда для отоброжения списка базы данных
     //datname : [template1, template0, postgres, sqlCmd]
 
 
