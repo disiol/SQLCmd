97be2ab6d4d693c51a697b5d41ecf0379ea215a7
Denis <deoniisii@gmail.com>
2018-01-25 14:49:02 +0200

    implements  interface Command

--- src/main/java/ua/com/denisimusIT/SQLCmd/controller/command/NameOfСurrentDatabase.java
+++ src/main/java/ua/com/denisimusIT/SQLCmd/controller/command/NameOfСurrentDatabase.java
@@ -1,6 +1,25 @@
 package ua.com.denisimusIT.SQLCmd.controller.command;
 
-public class NameOfСurrentDatabase {
+public class NameOfСurrentDatabase implements Command {
+    @Override
+    public boolean canProcess(String command) {
+        return false;
+    }
+
+    @Override
+    public void process(String command) {
+
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
+
+    @Override
+    public String format() {
+        return null;
+    }
     //TODO
     //current_database
     //  ------------------
