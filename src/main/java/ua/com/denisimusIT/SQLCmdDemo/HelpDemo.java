53be2455546d4338d2b391a09a2f6a414ab9896f
Denis <deoniisii@gmail.com>
2018-01-26 17:37:47 +0200

    dell ByteArrayOutputStream;

--- src/main/java/ua/com/denisimusIT/SQLCmdDemo/HelpDemo.java
+++ src/main/java/ua/com/denisimusIT/SQLCmdDemo/HelpDemo.java
@@ -2,15 +2,11 @@ package ua.com.denisimusIT.SQLCmdDemo;
 
 import ua.com.denisimusIT.SQLCmd.controller.Main;
 
-import java.io.ByteArrayOutputStream;
-
 
 public class HelpDemo {
     private static final String NEWLINE = System.lineSeparator();
 
     private static ConfigurableInputStream in;
-    private static ByteArrayOutputStream out;
-
 
     public static void main(String[] args) {
         System.out.println("HelpDemo" + NEWLINE);
